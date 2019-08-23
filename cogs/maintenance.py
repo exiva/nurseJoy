d311062ed02f8795b3b82f53506f39983f4817c8
Travis La Marr <exiva@exiva.net>
2019-08-23 00:25:32 -0400

    Add maintenance cog

--- cogs/maintenance.py
+++ cogs/maintenance.py
@@ -3,40 +3,44 @@
 from discord.ext import commands
 import discord
 
-class Newcog:
-    """The description for Newcog goes here."""
+
+class Maintenance(commands.Cog):
+    """
+        Maintaince tasks run by Nurse Joy
+
+    """
 
     def __init__(self, bot):
         self.bot = bot
 
-    def __unload(self):
+    def cog_unload(self):
         # clean up logic goes here
         pass
 
-    async def __local_check(self, ctx):
+    async def cog_check(self, ctx):
         # checks that apply to every command in here
         return True
 
-    async def __global_check(self, ctx):
+    async def bot_check(self, ctx):
         # checks that apply to every command to the bot
         return True
 
-    async def __global_check_once(self, ctx):
+    async def bot_check_once(self, ctx):
         # check that apply to every command but is guaranteed to be called only once
         return True
 
-    async def __error(self, ctx, error):
+    async def cog_command_error(self, ctx, error):
         # error handling to every command in here
         pass
 
-    async def __before_invoke(self, ctx):
+    async def cog_before_invoke(self, ctx):
         # called before a command is called here
         pass
 
-    async def __after_invoke(self, ctx):
+    async def cog_after_invoke(self, ctx):
         # called after a command is called here
         pass
 
 
 def setup(bot):
-    bot.add_cog(Newcog(bot))
+    bot.add_cog(Maintenance(bot))
