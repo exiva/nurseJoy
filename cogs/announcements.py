8e9355103f33327bd76b2bc832e5e8a7b3b58a7e
Travis La Marr <exiva@exiva.net>
2019-08-23 01:03:46 -0400

    Modernize announcements cog
    Switch to using new tasks helper

--- cogs/announcements.py
+++ cogs/announcements.py
@@ -3,12 +3,12 @@
 import asyncio
 import discord
 import urllib.request
-from discord.ext import commands
+from discord.ext import tasks, commands
 from distutils.version import StrictVersion
 from twitter import *
 
 
-class annoncements(commands.Cog):
+class announcements(commands.Cog):
     """Announcements cog
 
        Posts Tweet announcements, and version forces
@@ -27,49 +27,54 @@ class annoncements(commands.Cog):
             ['chrales', True, None]
             ]
         self.currentVersion = "0.0.0"
+        self.checkTweets.start()
+        self.checkVersionForce.start()
 
     def cog_unload(self):
-        print("Unloading announcements module...")
-        self.tweetTask.cancel()
-        self.versionTask.cancel()
-
+        print("Unloading announcements cog...")
+        self.checkTweets.cancel()
+        self.checkVersionForce.cancel()
         pass
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.announcements = self.bot.get_channel(345910796016156676)
-        self.tweetTask = self.bot.loop.create_task(self.checkTweets())
-        self.versionTask = self.bot.loop.create_task(self.checkVersionForce())
+        self.announcements = discord.utils.get(
+            self.bot.get_all_channels(),
+            guild__id=339074243838869504,
+            name="lab-1",
+        )
 
+    @tasks.loop(seconds=30)
     async def checkTweets(self):
-        try:
-            while not self.bot.is_closed():
-                for user in self.twitters:
-                    tweets = self.t.statuses.user_timeline(screen_name="PokemonGoApp")
-                    if not user[2]:
-                        user[2] = tweets[0]['id_str']
-                    elif user[2] != tweets[0]['id_str'] and tweets[0]['in_reply_to_screen_name'] is None:
-                        msg = f"https://twitter.com/{user[0]}/status/{tweets[0]['id_str']}"
-                        if user[1]:  # Spoiler tweet
-                            msg = f"|| {msg} ||"
-                        await self.announcements.send(msg)
-                    user[2] = tweets[0]['id_str']
-                await asyncio.sleep(30)
-        except Exception as e:
-            print(e)
+        for user in self.twitters:
+            tweets = self.t.statuses.user_timeline(screen_name=user[0])
+            if not user[2]:
+                user[2] = tweets[0]['id_str']
+            elif user[2] != tweets[0]['id_str'] and tweets[0]['in_reply_to_screen_name'] is None:
+                msg = f"https://twitter.com/{user[0]}/status/{tweets[0]['id_str']}"
+                if user[1]:  # Spoiler tweet
+                    msg = f"|| {msg} ||"
+                await self.announcements.send(msg)
+            user[2] = tweets[0]['id_str']
 
+    @tasks.loop(minutes=5)
     async def checkVersionForce(self):
-        try:
-            while not self.bot.is_closed():
-                with urllib.request.urlopen('https://pgorelease.nianticlabs.com/plfe/version') as version:
-                    version = version.read().decode('utf-8')[2:]
-                    if self.currentVersion != "0.0.0" and StrictVersion(self.currentVersion) < StrictVersion(version):
-                        await self.announcements.send(f"Pokemon Go update forced to version {version}. Check for updates in your App Store.")
-                    self.currentVersion = version
-                    await asyncio.sleep(300)
-        except Exception as e:
-            print(e)
+        with urllib.request.urlopen('https://pgorelease.nianticlabs.com/plfe/version') as version:
+            version = version.read().decode('utf-8')[2:]
+            if self.currentVersion != "0.0.0" and StrictVersion(self.currentVersion) < StrictVersion(version):
+                await self.announcements.send(f"Pokemon Go update forced to version {version}. Check for updates in your App Store.")
+            self.currentVersion = version
+            await asyncio.sleep(300)
+
+    # wait for bot to connect before starting tasks
+    @checkTweets.before_loop()
+    async def before_checktweets(self):
+        await self.bot.wait_until_ready()
+
+    @checkVersionForce.before_loop()
+    async def before_checkVersionForce(self):
+        await self.bot.wait_until_ready()
 
 
 def setup(bot):
-    bot.add_cog(annoncements(bot))
+    bot.add_cog(announcements(bot))
