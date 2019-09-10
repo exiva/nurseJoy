87cebefa2bc39e5504fa043de7ce4ca731cf6d9c
Travis La Marr <exiva@exiva.net>
2019-09-10 01:12:32 -0400

    Initial import user commands cog

---
+++ cogs/user.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from discord.ext import commands
+import discord
+
+class User(commands.Cog):
+    """The description for User goes here."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def cog_unload(self):
+        # clean up logic goes here
+        pass
+
+    async def cog_check(self, ctx):
+        # checks that apply to every command in here
+        return True
+
+    async def bot_check(self, ctx):
+        # checks that apply to every command to the bot
+        return True
+
+    async def bot_check_once(self, ctx):
+        # check that apply to every command but is guaranteed to be called only once
+        return True
+
+    async def cog_command_error(self, ctx, error):
+        # error handling to every command in here
+        pass
+
+    async def cog_before_invoke(self, ctx):
+        # called before a command is called here
+        pass
+
+    async def cog_after_invoke(self, ctx):
+        # called after a command is called here
+        pass
+
+
+def setup(bot):
+    bot.add_cog(User(bot))
