2fb620ff9573437d6248f3ab07a8e25b30470890
Travis La Marr <exiva@exiva.net>
2019-09-19 22:37:47 -0400

    Suppress commandNotFound exceptions

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 
 import discord
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 import config
 
 cogs = [
@@ -33,6 +34,9 @@ class Bot(commands.Bot):
     print('Logged on as {0} (ID: {0.id})'.format(self.user))
     await bot.change_presence(activity=discord.Game(name='at the Pokémon Center!'))
 
+  async def on_command_error(self, ctx, error):
+      if isinstance(error, CommandNotFound):
+          pass
 
 bot = Bot()
 bot.run(config.token)
