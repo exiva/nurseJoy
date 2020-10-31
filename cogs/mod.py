ac29fecba3c1dc1384d4d36757fb93e8c3e7556a
Travis La Marr <exiva@exiva.net>
2020-10-31 02:41:54 -0400

    Promote kick/ban commands to Mod level

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import aiohttp
 import pytz
 from discord.ext import commands
 from datetime import datetime
+from inspect import cleandoc
 import discord
 import re
 import io
@@ -231,6 +233,78 @@ class Mod(commands.Cog):
       if c.is_news():
         await msg.publish()
 
+  @commands.command()
+  async def kick(self, ctx, member: discord.Member, *, reason=None):
+    try:
+      if reason is None:
+        await ctx.send(
+            f"{ctx.author.mention}, you must specify a reason for the action."
+        )
+        return False
+
+      try:
+        await member.send(
+          cleandoc(
+              f"""Hello,
+                You were kicked from {ctx.message.guild} for **{reason}**. 
+                
+                You are allowed to rejoin the server if you wish to, but you may be banned in the future.
+                """
+          )
+        )
+      except discord.Forbidden:
+        self.logger.info(f"Cannot message {member}")
+
+      await ctx.guild.kick(member, reason=reason)
+      embed = discord.Embed(title="\U0001f97e User Kicked", color=int("ffff00", 16))
+      embed.add_field(name='Member', value=f'{member.display_name}', inline=False)
+      embed.add_field(name='Reason', value=f'{reason}', inline=False)
+      embed.set_footer(
+          icon_url=ctx.author.avatar_url,
+          text=
+          f"Kicked by {ctx.author.display_name} at {datetime.now().strftime('%d/%m/%y %I:%M:%S')}"
+      )
+      await ctx.send(embed=embed)
+
+      await member.send(
+          cleandoc(
+              f"""Hello,
+                You were kicked from {ctx.message.guild} for **{reason}**. You are allowed to rejoin the server if you wish to. But you may be banned in the future.
+                """
+          )
+      )
+
+    except Exception as e:
+      self.logger.error(e)
+
+  @commands.command()
+  async def ban(self, ctx, member: discord.Member, days: int = 0, *, reason=None):
+    try:
+      if reason is None:
+        await ctx.send(
+            f"{ctx.author.mention}, you must specify a reason for the ban."
+        )
+        return False
+
+      try:
+        await member.send(f"You were banned from {ctx.message.guild} for **{reason}**.")
+      except discord.Forbidden:
+        self.logger.info(f"Cannot message {member}")
+      
+      await ctx.guild.ban(user=member, reason=reason, delete_message_days=days)
+      embed = discord.Embed(title="\U0001f97e User Banned", color=int("ff0000", 16))
+      embed.add_field(name='Member', value=f'{member.display_name}', inline=False)
+      embed.add_field(name='Reason', value=f'{reason}', inline=False)
+      embed.set_footer(
+          icon_url=ctx.author.avatar_url,
+          text=
+          f"Kicked by {ctx.author.display_name} at {datetime.now().strftime('%d/%m/%y %I:%M:%S')}"
+      )
+      await ctx.send(embed=embed)
+
+
+    except Exception as e:
+      self.logger.error(e)
 
 def setup(bot):
   bot.add_cog(Mod(bot))
--- cogs/system.py
+++ cogs/system.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-from inspect import cleandoc
 import typing
 import pytz
 from discord.ext import commands
@@ -116,105 +115,6 @@ class System(commands.Cog):
       return
     else:
       await ctx.channel.purge(bulk=True, limit=9000, check=deleteCheck)
-
-  @commands.command()
-  async def kick(self, ctx, member: discord.Member, *, reason=None):
-    try:
-      if reason is None:
-        await ctx.send(
-            f"{ctx.author.mention}, you must specify a reason for the action."
-        )
-        return False
-
-      await ctx.guild.kick(member, reason=reason)
-      embed = discord.Embed(title="\U0001f97e User Kicked", color=int("ffff66", 16))
-      embed.add_field(name='Member', value=f'{member.display_name}', inline=False)
-      embed.add_field(name='Reason', value=f'{reason}', inline=False)
-      embed.set_footer(
-          icon_url=ctx.author.avatar_url,
-          text=
-          f"Kicked by {ctx.author.display_name} at {datetime.now().strftime('%d/%m/%y %I:%M:%S')}"
-      )
-      await ctx.send(embed=embed)
-
-      await member.send(
-          cleandoc(
-              f"""Hello,
-                You were kicked from {ctx.message.guild} for **{reason}**. You are allowed to rejoin the server if you wish to. But you may be banned in the future.
-                """
-          )
-      )
-
-    except Exception as e:
-      self.logger.error(e)
-
-  @commands.command()
-  async def ban(
-      self,
-      ctx,
-      members: commands.Greedy[discord.Member],
-      days: typing.Optional[int] = 0,
-      *,
-      reason=None
-  ):
-    try:
-      if reason is None:
-        await ctx.send(
-            f"{ctx.author.mention}, you must specify a reason for the action."
-        )
-        return False
-
-      for member in members:
-        await ctx.guild.ban(
-            discord.Object(id=member), reason=reason, delete_message_days=days
-        )
-        embed = discord.Embed(title="\u26d4 User Banned", color=int("ffff66", 16))
-        embed.add_field(name='Member', value=f'{member.display_name}', inline=False)
-        embed.add_field(name='Reason', value=f'{reason}', inline=False)
-        if days > 0:
-          embed.add_field(name='Messages Removed', value=f'{days} days', inline=False)
-        embed.set_footer(
-            icon_url=ctx.author.avatar_url,
-            text=
-            f"Banned by {ctx.author.display_name} at {datetime.now().strftime('%d-%m-%y %I:%M:%S')}"
-        )
-        await ctx.send(embed=embed)
-        await member.send(
-            cleandoc(
-                f"""Hello,
-                    You were banned from {ctx.message.guild} for **{reason}**.
-                    """
-            )
-        )
-    except Exception as e:
-      self.logger.error(e)
-
-  @commands.command()
-  async def mute(self, ctx, member: discord.Member, time: int = 10, *, reason=None):
-    log_chn = discord.utils.get(
-        self.bot.get_all_channels(),
-        guild__id=339074243838869504,
-        name="member-log",
-      )
-
-    embed = discord.Embed(title='Member Muted', color=discord.Colour.red())
-    embed.add_field(name='Muted Member', value=f"{member.name}#{member.discriminator}")
-    embed.add_field(name='Muted By', value=ctx.message.author)
-    embed.add_field(name='Length', value=time)
-    embed.add_field(name='Reason', value=f"{reason if reason else 'None Provided'}")
-
-    for c in ctx.guild.channels:
-      await c.set_permissions(member,
-            send_messages=False,
-            add_reactions=False
-            )
-    
-    await log_chn.send(embed=embed)
-    await ctx.send(f"{member.mention} you have been muted for {time} minutes.")
-
-    await asyncio.sleep(time * 60)
-    for c in ctx.guild.channels:
-      await c.set_permissions(member, overwrite=None)
 
 def setup(bot):
   bot.add_cog(System(bot))
