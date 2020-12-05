5ba2c162dfd905b67a432fdb1eac524a5edf5634
Travis La Marr <exiva@exiva.net>
2020-12-05 17:47:36 -0500

    Fix commands Nudge people to assign a team when using hemlock

--- cogs/fuckinghemlock.py
+++ cogs/fuckinghemlock.py
@@ -16,11 +16,6 @@ class fuckingHemlock(commands.Cog):
     self.logger = bot.logger
     self.logger.info(f"Loaded {self.__class__.__name__} cog")
     self.bot = bot
-    self.raidcommands = ["i","interested","interest","hmm","join","j","attend","omw","coming",
-    "cominf","going","here","h","herr","arrive","arrived","present","check-in",
-    "physically-here","physically-present","checkin","physicallyhere","physicallypresent",
-    "please","p","please-invite","invite-pls","invitepls","invite-plz","inviteplz",
-    "plz","pls","pleaseinvite","invitepls","inviteplz","remote","r","home"]
   
   @commands.Cog.listener()
   async def on_guild_channel_create(self, channel):
@@ -50,13 +45,30 @@ class fuckingHemlock(commands.Cog):
       await asyncio.sleep(1)
       await welcomeChannel.edit(position=0)
 
-  @commands.command(hidden=True, aliases=self.raidcommands)
+  @commands.command(hidden=True, aliases=["i","interested","interest","hmm","join","j","attend","omw","coming",
+    "cominf","going","here","h","herr","arrive","arrived","present","check-in",
+    "physically-here","physically-present","checkin","physicallyhere","physicallypresent",
+    "please","p","please-invite","invite-pls","invitepls","invite-plz","inviteplz",
+    "plz","pls","pleaseinvite","inviteplz","remote","r","home"])
   async def maybe(self, ctx):
     teams = set(['instinct', 'mystic', 'valor'])
     roles = set([r.name.lower() for r in ctx.message.author.roles])
     if not teams.intersection(roles):
       await ctx.send(f"{ctx.message.author.mention}, you don't have a team set. Please set your team by sending `!team instinct, mystic, or valor`.")
 
-
+  @commands.Cog.listener()
+  async def on_raw_reaction_add(self, payload):
+    if payload.user_id in [448855958928752640, 448855673623805966]:
+      return
+    emojis = [687744604552167429, 687744744851243052, 687744766720344084, 742380909428342934, 704410930288787516]
+    if payload.emoji.id in emojis and payload.event_type == "REACTION_ADD":
+      channel = self.bot.get_guild(payload.guild_id).get_channel(payload.channel_id)
+      message = await channel.fetch_message(payload.message_id)
+      user = message.guild.get_member(payload.user_id)
+      teams = set(['instinct', 'mystic', 'valor'])
+      roles = set([r.name.lower() for r in user.roles])
+      if not teams.intersection(roles):
+        await channel.send(f"{user.mention}, you don't have a team set. Please set your team by sending `!team instinct, mystic, or valor`.")
+  
 def setup(bot):
   bot.add_cog(fuckingHemlock(bot))
